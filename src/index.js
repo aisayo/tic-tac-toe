909d3aa89e1e6ad00771badbb82b3c7066b91d6d
Aysan Isayo <aisayo@icloud.com>
2018-08-23 16:54:13 -0700

    finished

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import App from './app';
+
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+registerServiceWorker();
+  
+  
+  
